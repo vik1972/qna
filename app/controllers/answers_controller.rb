1ffdf8ddaa52e916456b231863e0e872faedf1e1
viktor <bvv33@yandex.ru>
2022-04-24 14:08:30 +0200

    Created AnswersController #create with tests

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,25 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[create]
+
+  def new
+
+  end
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
